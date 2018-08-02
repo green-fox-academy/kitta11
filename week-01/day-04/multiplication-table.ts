9296ebd10f92637b900262c29d1fa4a7e354abe3
PKriszta <parrag.krisztina@gmail.com>
2018-08-02 16:54:30 +0200

    commit includes the multiplication-table task

---
+++ week-01/day-04/multiplication-table.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let number: number = 15;
+
+// Create a program that
+// prints the multiplication table with number
+
+for (let index = 1; index < 11; index++) {
+    console.log(`${index} * ${number} = ${index * number}`);        
+}
