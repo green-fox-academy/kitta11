1244369448ea61e5ab5822d94e216ce667fe9cc4
PKriszta <parrag.krisztina@gmail.com>
2018-08-02 16:43:32 +0200

    commit includes the cheating taskn

---
+++ week-01/day-04/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (let cheatIndex = 0; cheatIndex < 100; cheatIndex++) {
+console.log(`I won't cheat on the exam!`);   
+}
