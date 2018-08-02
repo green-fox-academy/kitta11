9e9ceab37f10de0a6c3d37d97679a8426034395e
PKriszta <parrag.krisztina@gmail.com>
2018-08-02 16:50:01 +0200

    commit includes the print even task

---
+++ week-01/day-04/print-even.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+for (let index = 1; index < 501; index++) {
+    if (index % 2 === 0) {console.log(index);
+    } else {}
+    
+}
