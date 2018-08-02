57a6b7af268810e069e5318a532d1f009100ab11
PKriszta <parrag.krisztina@gmail.com>
2018-08-02 17:00:20 +0200

    commit includes the amazing fizzbuzz task

---
+++ week-01/day-04/fizz-buzz.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of 3 print “Fizz” instead of the number
+// and for the multiples of 5 print “Buzz”.
+// For numbers which are multiples of both 3 and 5 print “FizzBuzz”.
+
+for (let index = 1; index < 100; index++) {
+    if ( index % 3 === 0 && index % 5 === 0) {
+        console.log('FizzBuzz');        
+    } else if (index % 3 === 0) {
+        console.log('Fizz');
+    } else if ( index % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(index);
+    }
+}
