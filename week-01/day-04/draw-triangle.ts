ec78068d09bf8d77fdafca330db190286bd0fe1c
PKriszta <parrag.krisztina@gmail.com>
2018-08-02 17:44:29 +0200

    commit includes the current status of draw triangle, but not solved yet

---
+++ week-01/day-04/draw-triangle.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+let lineCount: number = 4;
+
+
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+for (let index = 1; index <= lineCount; index++) {
+    let str[index]= new String ('*');
+    
+}
+
+//does not work in TS    console.log('*'.repeat(count: index));    
+
+
