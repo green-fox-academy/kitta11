a76173702de93b31c3af91bc6365dd251a75278b
PKriszta <parrag.krisztina@gmail.com>
2018-08-03 14:43:55 +0200

    updated pyramid

--- week-01/day-05/draw_pyramid.ts
+++ week-01/day-05/draw_pyramid.ts
@@ -1,12 +1,14 @@
 'use strict';
 
-let lineCount2: number = 4;
+let lineCount2: number = 40;
 
-let space1: string = '---';
+let space1: string = '';
+for (let j = 0; j < Math.floor(lineCount2/2); j++) {
+    space1 = space1 + '-';
+}
 let starsCenter: string = '*';
-console.log(space1 + starsCenter + space1);
 
-for (let index = 1; index < lineCount2; index++) {
+for (let index = 0; index < lineCount2/2; index++) {
     space1 = space1.replace('-','');
     starsCenter = '*' + starsCenter + '*';
     console.log(space1 + starsCenter + space1);
