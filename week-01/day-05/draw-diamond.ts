afc27a5e4020269a98a513753b3cc730976df541
PKriszta <parrag.krisztina@gmail.com>
2018-08-03 14:30:17 +0200

    commit contains a finetuned diamond model which now calculate the start parameter dynamically

--- week-01/day-05/draw-diamond.ts
+++ week-01/day-05/draw-diamond.ts
@@ -1,13 +1,17 @@
 'use strict';
-let lineCountDiamond: number = 7;
+let lineCountDiamond: number = 20;
 
 
-let sideBlock: string = '---';
+let sideBlock: string = '' 
+for (let j = 0; j < Math.floor(lineCountDiamond/2); j++) {
+    sideBlock = sideBlock + '-';
+}
 let diamondStar: string = '*';
+
 console.log(sideBlock + diamondStar + sideBlock);
 
-for (let index = 1; index < lineCountDiamond; index++) {
-    if (index <= Math.floor(lineCountDiamond/2)) {
+for (let index = 0; index < lineCountDiamond; index++) {
+    if (index <= (Math.ceil(lineCountDiamond/2)-1)) {
     sideBlock = sideBlock.replace('-','');
     diamondStar = '*' + diamondStar + '*';
     console.log(sideBlock + diamondStar + sideBlock);
