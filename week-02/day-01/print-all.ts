d8e3a969bee26f091a7d8e28313485dc2554c5a6
PKriszta <parrag.krisztina@gmail.com>
2018-08-06 16:20:20 +0200

    this commit contains the print-all task

---
+++ week-02/day-01/print-all.ts
@@ -0,0 +1,15 @@
+'use strict';
+// -  Create a variable named `integers` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `integers` to the console*
+// *hint: use a loop, console.log(integers) won't cut it
+// -  bonus for using the correct built in array method
+
+let integers = [4, 5, 6, 7];
+
+//using a loop
+for (let index = 0; index < integers.length; index++) {
+    console.log(integers[index]);
+}
+
+//using the built in array method
+console.log(integers.toString());
