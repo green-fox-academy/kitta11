cf6cf9c262ce8897f3edadba874fd74811a38ba3
PKriszta <parrag.krisztina@gmail.com>
2018-08-06 14:25:18 +0200

    Commit contains the summing exercise

---
+++ week-02/day-01/append-a-func.ts
@@ -0,0 +1,13 @@
+
+'use strict'
+export {};
+// -  Create a string variable named `typo` and assign the value `Chinchill` to it
+// -  Write a function called `appendAFunc` that gets a string as an input,
+//    appends an 'a' character to its end and returns with a string
+// -  Print the result of `appendAFunc(typo)`
+
+let typo: string = 'Chincill';
+function appendAFunc (inputString: string): string {
+    return inputString + 'a'
+}
+console.log(appendAFunc(typo));
---
+++ week-02/day-01/sum.ts
@@ -0,0 +1,16 @@
+'use strict';
+export {};
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
+
+function sum (untilWhenSum: number): number {
+    let total = 0
+    for (let i = 1; i <= untilWhenSum; i++) {
+        total = total + i; 
+    }
+    return total;   
+}
+let untilWhenSum: number = Math.floor(Math.random() * 100);
+console.log(`The computer wants me to sum numbers from 1 to ${untilWhenSum}
+the sum of these numbers is ${sum(untilWhenSum)}
+I am unbelievably smart... :)`);
