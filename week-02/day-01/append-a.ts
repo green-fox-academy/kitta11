c4958c16cfa4b5a2680e96cb12e7d6db32f5f8be
PKriszta <parrag.krisztina@gmail.com>
2018-08-06 17:16:47 +0200

    this commit contains the append the list elements with a string task

---
+++ week-02/day-01/append-a.ts
@@ -0,0 +1,11 @@
+'use strict';
+export {};
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals: string[] = ["koal", "pand", "zebr"];
+for (let i = 0; i < animals.length; i++) {
+    animals[i] = animals[i]+'a';     
+}
+console.log(animals);
