5bcc58764842494959c5944e0cdc612795f967b1
PKriszta <parrag.krisztina@gmail.com>
2018-08-07 16:23:32 +0200

    this commit contains the :candy: candyshop task solution

---
+++ week-02/day-02/candyshop.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+let shopItems: any[] = ['Cupcake', 2, 'Brownie', false];
+
+// Accidentally we added "2" and "false" to the array.
+// Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+// No, don't just remove the items :)
+// Create a function called sweets() which takes the list as a parameter.
+// Expected output: "Cupcake", "Croissant", "Brownie", "Ice cream"
+
+function correction (inputArray: any []) {
+    let newItemOne: string = 'Croissant';
+    let newItemTwo: string = 'Ice cream';
+    let errorIndexOne: number = inputArray.indexOf(2);
+    let errorIndexTwo: number = inputArray.indexOf(false);
+    inputArray.splice(errorIndexOne, 1, newItemOne);
+    inputArray.splice(errorIndexTwo, 1, newItemTwo);
+    return console.log(inputArray);
+}
+
+correction(shopItems);
+
