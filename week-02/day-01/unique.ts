09fadd99066eee6be7bac33d794fe47569f95cf8
PKriszta <parrag.krisztina@gmail.com>
2018-08-09 13:01:39 +0200

    this commit contains a second solution to unique however still using an external solution to solve the SORT does sorting in alphabetical order regardless we want to sort numbers

--- week-02/day-01/unique.ts
+++ week-02/day-01/unique.ts
@@ -2,12 +2,12 @@
 //  Create a function that takes a list of numbers as a parameter
 //  Returns a list of numbers where every number in the list occurs only once
 
-let testArray: number[] = [34, 1, 11, 34, 11, 52, 61, 1, 34];
+let testArray: number[] = [34, 1, 111, 34, 110, 52, 12, 61, 1, 112, 34];
 
-//I have found this onlyUnique function on the net, tested it and since it worked I used it
+//FIRST STEPS: I have found this onlyUnique function on the net, tested it and since it worked I used it
 
 function unique(inputArray: any[]) {
-//this is the magical function
+    //this is the magical function
     function onlyUnique(value, index, self) {
         return self.indexOf(value) === index;
     }
@@ -19,5 +19,25 @@ console.log(unique(testArray));
 let testArray2: string[] = ['Soma', 'Kinga', 'Soma', 'Kitta', 'Kinga', 'Soma', 'soma'];
 console.log(unique(testArray2));
 
+//AND THE HEUREKA MOMENT
 
+function sorter(a, b) {
+    if (a < b) return -1;  // any negative number works
+    if (a > b) return 1;   // any positive number works
+    return 0; // equal values MUST yield zero
+}
+
+function myUnique(inputArray: number[]) {
+    inputArray.sort(sorter);
+    let uniqueArray: number[] = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i + 1] !== inputArray[i]) {
+            uniqueArray.push(inputArray[i]);
+        }
+    }
+    return uniqueArray;
+}
+let arrTest2: number[] = [9, 9, 111.0, 2, 3, 4, 4, 5, 7];
+console.log(myUnique(arrTest2));
+console.log(myUnique(testArray));
 
