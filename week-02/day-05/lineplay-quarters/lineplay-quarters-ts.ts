77103d973a06200cf577ecebdba3dd0f1f89dc26
PKriszta <parrag.krisztina@gmail.com>
2018-08-10 12:06:26 +0200

    (feat) quarterlines with 4 for loops

---
+++ week-02/day-05/lineplay-quarters/lineplay-quarters-ts.ts
@@ -0,0 +1,38 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+const halfCanvas: number = canvas.width / 2;
+const divider: number = 40;
+let distance: number = halfCanvas / divider;
+console.log(distance);
+console.log(halfCanvas / divider);
+
+function drawLine(xStart: number, yStart: number, xEnd: number, yEnd: number, color) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.moveTo(xStart, yStart);
+    ctx.lineTo(xEnd, yEnd);
+    ctx.stroke();
+}
+
+//Note: if I set i=0 than I have line in the middle but I need to set i<=div too
+
+for (let i = 0; i <= divider; i++) {
+    drawLine(halfCanvas - i * distance, halfCanvas, halfCanvas, i * distance, "pink");
+}
+
+for (let i = 0; i <= divider; i++) {
+    drawLine(halfCanvas, i * distance, i * distance + halfCanvas, halfCanvas, "pink");
+}
+
+for (let i = 0; i <= divider; i++) {
+    drawLine(i * distance, halfCanvas, halfCanvas, halfCanvas + i * distance, "pink");
+}
+
+for (let i = 1; i <= divider; i++) {
+    drawLine(i * distance + halfCanvas, halfCanvas, halfCanvas, halfCanvas * 2 - i * distance, "pink");
+}
