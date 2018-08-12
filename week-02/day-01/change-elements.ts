ed463ce2595e080564e91527589700f2cb3a0e11
PKriszta <parrag.krisztina@gmail.com>
2018-08-12 18:35:27 +0200

    (fix) fixed splice version and finetuned map solution for replacing 8 for 4

--- week-02/day-01/change-elements.ts
+++ week-02/day-01/change-elements.ts
@@ -1,5 +1,5 @@
 'use strict';
-export {};
+export { };
 
 // -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
 // -  Change the 8 to 4 with the `.map` method 
@@ -8,11 +8,13 @@ export {};
 let numList = [1, 2, 3, 8, 5, 6];
 
 //solving the problem with splice method
-//numList.splice(3, 1, 4);
-//console.log(numList);
+numList.splice(numList.indexOf(8), 1, 4);
+console.log(numList);
 
-let changedNumList = numList.map(function(elem:number) {
-    return elem/2;
-});
+let numListTwo = [1, 2, 3, 8, 5, 6];
 
-console.log(changedNumList[3]);
+//I do not like map grrr
+let changedNumList: number [] = numListTwo.map(function(element) {
+    return element/2;
+})
+console.log(changedNumList);
