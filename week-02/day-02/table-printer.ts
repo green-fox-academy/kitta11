e4984ae747ab5245a87462ace991a0aa1f0a0bfb
PKriszta <parrag.krisztina@gmail.com>
2018-08-12 22:51:33 +0200

    (feat) upper line, table head + a half ingredient printer

--- week-02/day-02/table-printer.ts
+++ week-02/day-02/table-printer.ts
@@ -1,4 +1,5 @@
 'use strict';
+export { };
 // Create a function that prints the ingredient list of dictionaries to the console in the following format:
 //
 // +--------------------+---------------+----------+
@@ -13,6 +14,7 @@
 //
 // OPTIONAL
 // The frist columns should be automatically as wide as the longest key
+
 const ingredients: any[] = [
 	{ name: 'vodka', inStock: 1, needsCooling: true },
 	{ name: 'coffee_liqueur', inStock: 0, needsCooling: true },
@@ -23,3 +25,26 @@ const ingredients: any[] = [
 	{ name: 'lime juice', inStock: 0, needsCooling: true },
 	{ name: 'soda', inStock: 0, needsCooling: true }
 ];
+function printTheIngr(inputArray: object[]) {
+	let colOne: string = 'Ingredient';
+	let colTwo: string = 'Needs cooling';
+	let colThree: string = 'In stock';
+	let separatorOne: string = '-';
+	let separatorTwo: string = '|'
+	let separatorThree: string = '+';
+
+	let tableLine: string = separatorThree + Array(20).join(separatorOne) + separatorThree + Array(15).join(separatorOne) + separatorThree + Array(10).join(separatorOne) + separatorThree;
+	console.log(tableLine);
+	let coloumHead: string = separatorTwo + ' ' + colOne + Array(20 - colOne.length - 1).join(' ') + separatorTwo + ' ' + colTwo + Array(15 - colTwo.length - 1).join(' ') + separatorTwo + ' ' + colThree + Array(10 - colThree.length - 1).join(' ') + separatorTwo;
+	console.log(coloumHead);
+	console.log(tableLine);
+	let lineRow: string = '';
+	for (let i = 0; i < ingredients.length; i++) {
+		lineRow = separatorTwo + ' ' + inputArray[i]['name'] + Array(20 - inputArray[i]['name'].length - 1).join(' ') + separatorTwo + ' ' + inputArray[i]['needsCooling'] + Array(15 - inputArray[i]['needsCooling'].length - 1).join(' ') + separatorTwo + ' ' + inputArray[i]['inStock'] + Array(10 - inputArray[i]['inStock'].length - 1).join(' ') + separatorTwo;
+	}
+	console.log(lineRow);
+	lineRow = '';
+	console.log(tableLine);
+}
+
+printTheIngr(ingredients);
