c00002af5f4a939402d6e6b27e91c1fd9322d402
PKriszta <parrag.krisztina@gmail.com>
2018-08-14 16:51:14 +0200

    (feat) function that is able to filter the ip addresses from a text file based on regexp

---
+++ week-03/day-02/logs.ts
@@ -0,0 +1,28 @@
+'use strict';
+export { };
+// Read all data from 'log.txt'.
+// Each line represents a log message from a web server
+// Write a function that returns an array with the unique IP adresses.
+// Write a function that returns the GET / POST request ratio.
+
+const fs = require('fs');
+const charEncoding: string = 'utf8'
+
+//general functions to read and write a file
+function readFromFile(filename: string): string {
+  return fs.readFileSync(filename, charEncoding);
+}
+
+function getIPAddress(filename: string) {
+  let fileContentArray: any[] = readFromFile(filename).split('\n');
+  let ipArray: any[] = [];
+  fileContentArray.forEach(element => {
+    //Regular Expression to Match IP Addresses: ([0-9]{1,3}[\.]){3}[0-9]{1,3}
+    //https://javascript.info/regexp-methods When there’s a "g" flag, then str.match returns an array of all matches
+    ipArray.push(element.toString().match(/([0-9]{1,3}[\.]){3}[0-9]{1,3}/ig));
+  });
+  return ipArray;
+}
+
+console.log(getIPAddress('log.txt'));
+
