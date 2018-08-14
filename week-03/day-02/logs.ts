088765cb6597fcf8f38693411e9fdfea78ef335d
PKriszta <parrag.krisztina@gmail.com>
2018-08-14 17:05:59 +0200

    (FEAT) function to get the GET/POST ratio and and extra test file log2.txt to check the math

--- week-03/day-02/logs.ts
+++ week-03/day-02/logs.ts
@@ -2,8 +2,6 @@
 export { };
 // Read all data from 'log.txt'.
 // Each line represents a log message from a web server
-// Write a function that returns an array with the unique IP adresses.
-// Write a function that returns the GET / POST request ratio.
 
 const fs = require('fs');
 const charEncoding: string = 'utf8'
@@ -12,6 +10,8 @@ const charEncoding: string = 'utf8'
 function readFromFile(filename: string): string {
   return fs.readFileSync(filename, charEncoding);
 }
+
+// Write a function that returns an array with the unique IP adresses.
 
 function getIPAddress(filename: string) {
   let fileContentArray: any[] = readFromFile(filename).split('\n');
@@ -24,5 +24,31 @@ function getIPAddress(filename: string) {
   return ipArray;
 }
 
-console.log(getIPAddress('log.txt'));
+//console.log(getIPAddress('log.txt'));
+console.log(getIPAddress('log2.txt'));
+
+
+// Write a function that returns the GET / POST request ratio.
+
+function getPostRatio(filename: string) {
+  let fileContentArray: any[] = readFromFile(filename).split('\n');
+  let getCount: number = 0;
+  let postCount: number = 0;
+  fileContentArray.forEach(element => {
+    if (element.toString().match(/GET/) === null) {
+      postCount++;
+    } else {
+      getCount++;
+    }
+})
+console.log(postCount);
+console.log(getCount);
+return getCount/postCount;
+}
+
+//console.log(getPostRatio('log.txt'));
+
+console.log(getPostRatio('log2.txt'));
+//should return 3/7 = 0.42
+
 
