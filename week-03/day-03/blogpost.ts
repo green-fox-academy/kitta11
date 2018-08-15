a34d1f055de983642be03ac7cf8f96d91705cf37
PKriszta <parrag.krisztina@gmail.com>
2018-08-15 14:13:27 +0200

    (feat) blogpost class

---
+++ week-03/day-03/blogpost.ts
@@ -0,0 +1,26 @@
+'use strict';
+export { };
+
+class Blogpost {
+  authorName: string;
+  title: string;
+  text: string;
+  publicationDate: string;
+  constructor(authorName: string, title: string, text: string, publicationDate: string) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+  }
+}
+
+let postOne: Blogpost = new Blogpost ('John Doe', 'Lorem Ipsum', 'Lorem ipsum dolor sit amet.', '2010.10.10');
+let postTwo: Blogpost = new Blogpost ('Tim Urban', 'Wait but why',  'A popular long-form, stick-figure-illustrated blog about almost everything.', '2010.10.10.'
+);
+let postThree: Blogpost = new Blogpost ('William Turton', 'One Engineer Is Trying to Get IBM to Reckon With Trump', 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.', '2017.03.28.'
+);
+
+console.log(postOne, postTwo, postThree);
+
+
+
