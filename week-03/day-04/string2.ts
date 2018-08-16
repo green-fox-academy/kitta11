9c81e110259c7553e49da6fa5794f9e2dd29ee18
PKriszta <parrag.krisztina@gmail.com>
2018-08-16 17:15:58 +0200

    (fix) code optimization on the return part

--- week-03/day-04/string2.ts
+++ week-03/day-04/string2.ts
@@ -1,20 +1,19 @@
 'use strict';
-export{};
+export { };
 
 // Given a string, compute recursively a new string where all the 'x' chars have been removed.
 
-function xDelete (inputString: string, letterX: string): string {
-  if (inputString.indexOf(letterX)=== -1) {
+function xDelete(inputString: string, letterX: string): string {
+  if (inputString.indexOf(letterX) === -1) {
     return inputString
-   } else {
-    inputString = inputString.replace(letterX, '')
-    return xDelete(inputString, letterX);
-   }
- }
+  } else {
+    return xDelete(inputString.replace(letterX, ''), letterX);
+  }
+}
 
- console.log(xDelete('oxremipxum', 'x'));
- console.log(xDelete('oxrexipxumytrx iuxiuy', 'x'));
- let quoteOne: string = "We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins."
+console.log(xDelete('oxremipxum', 'x'));
+console.log(xDelete('oxrexipxumytrx iuxiuy', 'x'));
+let quoteOne: string = "We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins."
 
- console.log(xDelete(quoteOne, 'i'));
- 
+console.log(xDelete(quoteOne, 'i'));
+
