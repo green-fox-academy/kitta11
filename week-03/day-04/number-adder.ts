138b3ed15b75d5cff784bbb90ce861953f2c9c40
PKriszta <parrag.krisztina@gmail.com>
2018-08-16 11:01:20 +0200

    (fix) support total variable has been removed

--- week-03/day-04/number-adder.ts
+++ week-03/day-04/number-adder.ts
@@ -3,13 +3,11 @@ export {};
 // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
 
 function addingNumbers (inputNumber: number): number {
-  let total: number = 0;
   if (inputNumber>0) {
-    total += inputNumber + addingNumbers(inputNumber-1);
+    return inputNumber + addingNumbers(inputNumber-1);
   } else {
     return 0;
   }
-return total;
 }
 
 console.log(addingNumbers(4));
