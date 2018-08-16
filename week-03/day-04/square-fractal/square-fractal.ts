60dcbf32830009af2df6c6e61276561c78a9bf27
PKriszta <parrag.krisztina@gmail.com>
2018-08-16 15:14:56 +0200

    (feat) 4square in a square function with a recursion

---
+++ week-03/day-04/square-fractal/square-fractal.ts
@@ -0,0 +1,31 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+let color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;    //ctx.strokeStyle = colorPalette[Math.floor(Math.random()*colorPalette.length)];  
+let canvasWidth: number = canvas.width;
+let canvasHeight: number = canvas.height;
+
+
+function generateSquareRec(xStart: number, yStart: number, startSize: number, loopNum: number) {
+  if (loopNum > 0) {
+    ctx.strokeStyle = 'green';
+    ctx.strokeRect(xStart + startSize, yStart, startSize, startSize);
+    ctx.strokeRect(xStart + 2 * startSize, yStart + startSize, startSize, startSize);
+    ctx.strokeRect(xStart, yStart + startSize, startSize, startSize);
+    ctx.strokeRect(xStart + startSize, yStart + 2 * startSize, startSize, startSize);
+    
+    startSize /= 3;
+    generateSquareRec(xStart+startSize*3, yStart, startSize, loopNum - 1);
+    generateSquareRec(xStart+startSize*6, yStart+startSize*3, startSize, loopNum - 1);
+    generateSquareRec(xStart+startSize*3, yStart+startSize*6, startSize, loopNum - 1);
+    generateSquareRec(xStart, yStart+startSize*3, startSize, loopNum - 1);
+
+  } else {
+  }
+}
+
+generateSquareRec(0, 0, canvasWidth/3, 5);
