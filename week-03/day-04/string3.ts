e0aa6ee79771d19b0a74f71578bf411d0417b112
PKriszta <parrag.krisztina@gmail.com>
2018-08-16 14:03:45 +0200

    (feat) adding an extra char after every letter with recursion

--- week-03/day-04/string3.ts
+++ week-03/day-04/string3.ts
@@ -4,12 +4,19 @@ export { };
 // Given a string, compute recursively a new string where all the
 // adjacent chars are now separated by a '*'.
 
-let teststring: string = 'kutyabajom';
+function addStarRecur (inputString: string): any {
+  if (inputString.length > 0) {
+    return inputString.slice(0, 1)+'*' + addStarRecur(inputString.slice(1));
+  } else {
+    return '';
+  }
+}
+
+console.log(addStarRecur('kutyabajom mert nem tudom vajon jol csinaltam'));
 
 
+//without recursion
 function addStar(inputString: string, separator: string): any {
   return inputString.split('').join(separator);
 }
-
-console.log(addStar('kutyabajom mert nem tudom vajon jol csinaltam', '*'));
 console.log(addStar('kutyabajom mert nem tudom vajon jol csinaltam', '-'));
