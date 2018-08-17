bd811225e40c710f5d15d2494cefb1d187da8fcd
PKriszta <parrag.krisztina@gmail.com>
2018-08-17 14:09:17 +0200

    (optim) I reduced the code size eliminating an unnecessary step

--- week-03/day-04/square-fractal/square-fractal.ts
+++ week-03/day-04/square-fractal/square-fractal.ts
@@ -5,26 +5,22 @@ const ctx = canvas.getContext('2d');
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
-let color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;    //ctx.strokeStyle = colorPalette[Math.floor(Math.random()*colorPalette.length)];  
 let canvasWidth: number = canvas.width;
 let canvasHeight: number = canvas.height;
 
-function generateSquareRec(xStart: number, yStart: number, startSize: number, loopNum: number) {
+function generateSquareRec(x: number, y: number, size: number, loopNum: number) {
   if (loopNum > 0) {
-    ctx.strokeStyle = 'purple';
-    ctx.strokeRect(xStart + startSize, yStart, startSize, startSize);
-    ctx.strokeRect(xStart + 2 * startSize, yStart + startSize, startSize, startSize);
-    ctx.strokeRect(xStart, yStart + startSize, startSize, startSize);
-    ctx.strokeRect(xStart + startSize, yStart + 2 * startSize, startSize, startSize);
-    
-    startSize /= 3;
-    generateSquareRec(xStart+startSize*3, yStart, startSize, loopNum - 1);
-    generateSquareRec(xStart+startSize*6, yStart+startSize*3, startSize, loopNum - 1);
-    generateSquareRec(xStart+startSize*3, yStart+startSize*6, startSize, loopNum - 1);
-    generateSquareRec(xStart, yStart+startSize*3, startSize, loopNum - 1);
+    let color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;    //ctx.strokeStyle = colorPalette[Math.floor(Math.random()*colorPalette.length)];  
+    ctx.strokeStyle = color;
+    ctx.strokeRect(x, y, size, size);
 
-  } else {
+    size /= 3;
+    generateSquareRec(x + size, y, size, loopNum - 1);
+    generateSquareRec(x + 2 * size, y + size, size, loopNum - 1);
+    generateSquareRec(x + size, y + 2 * size, size, loopNum - 1);
+    generateSquareRec(x, y + size, size, loopNum - 1);
+
   }
 }
 
-generateSquareRec(0, 0, canvasWidth/3, 5);
+generateSquareRec(0, 0, canvasWidth, 5);
