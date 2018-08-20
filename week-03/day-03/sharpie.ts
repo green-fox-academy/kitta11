9b1f0af81c5eddac6129de330c90f02702a76658
PKriszta <parrag.krisztina@gmail.com>
2018-08-20 14:53:23 +0200

    (feat) sharpie class is created

---
+++ week-03/day-03/sharpie.ts
@@ -0,0 +1,28 @@
+import { userInfo } from "os";
+
+'use strict';
+
+export class Sharpie {
+  private color: string;
+  private width: number;
+  private inkAmount: number;
+
+  constructor(color: string, width: number) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = 100;
+  }
+
+  use () {
+    this.inkAmount--;
+  }
+}
+
+//test
+let item1: Sharpie = new Sharpie ('pink', 10);
+console.log(item1);
+let item2: Sharpie = new Sharpie ('red', 12);
+console.log(item2);
+item1.use()
+console.log(item1);
+
