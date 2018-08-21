2b62536a5e6de958b3c6edcc4e55b983cc4bb2a1
PKriszta <parrag.krisztina@gmail.com>
2018-08-21 14:07:06 +0200

    (bugfix) the addFlower and addTree method were finetunes in order to work like spec

--- week-04/day-02/garden-app/garden.ts
+++ week-04/day-02/garden-app/garden.ts
@@ -7,9 +7,12 @@ export class Garden {
   flowerList: Flower [];
   treeList: Tree [];
 
-addFlower(){
-  let newFlower: Flower = new Flower()
+addFlower(flowerName){
+  this.flowerList.push(flowerName)
+}
 
+addTree(treeName){
+  this.treeList.push(treeName)
 }
 
 }
