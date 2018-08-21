f119d3cd826a5e6f616f7029735c6f8a9e3f8e54
PKriszta <parrag.krisztina@gmail.com>
2018-08-21 13:11:25 +0200

    (feat) cohort class is created with basic properties and methods and program.ts is extended with cohort import

---
+++ week-04/day-02/greenfox-org/cohort.ts
@@ -0,0 +1,30 @@
+import { Student} from "./student";
+import { Mentor} from "./mentor"
+
+'use strict'
+
+export class Cohort {
+  name: string;
+  studentList: Student[];
+  mentorList: Mentor[];
+
+  constructor(name: string){
+    this.name = name;
+    this.studentList = [];
+    this.mentorList = [];
+  }
+  addStudent(Student) {
+    let newStudent: Student = new Student;
+    this.studentList.push(newStudent);
+  }
+
+  addMentor(Mentor) {
+    let newMentor: Mentor = new Mentor;
+    this.mentorList.push(newMentor);
+
+  }
+
+  info() {
+    console.log(`The name cohort has ${this.studentList.length} students and ${this.mentorList.length} mentors.`)
+  }
+}
