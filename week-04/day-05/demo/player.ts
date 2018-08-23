92e06994f4a6259c4e8096b672d30f6ac80410ea
PKriszta <parrag.krisztina@gmail.com>
2018-08-23 21:21:08 +0200

    (feat)player class created, throwdart method created, basic file structure done

---
+++ week-04/day-05/demo/player.ts
@@ -0,0 +1,51 @@
+'use strict';
+
+export class Player {
+  protected name: string;
+  protected level: number;
+  protected isFortunate: boolean;
+  protected alcoholLevel: number;
+  protected noOfRounds: number;
+  protected noOfWins: number;
+
+  constructor(name: string) {
+    this.name = name;
+    this.level = 1;
+    this.isFortunate = this.fortuneGen()
+    this.alcoholLevel = 0;
+    this.noOfRounds = 0;
+    this.noOfWins = 0;
+  }
+
+  fortuneGen(): boolean {
+    return (Math.floor(Math.random() * 2) < 1)
+  }
+
+  playerInfoCard() {
+    console.log(`
+    Name: ${this.name}
+    I am at ${this.level} level, my fortune is ${this.isFortunate},
+    I am at ${this.alcoholLevel}, I am over of ${this.noOfRounds} game rounds
+    and I won ${this.noOfWins}`)
+  }
+
+  getName() {
+    return this.name
+  }
+
+  getLevel() {
+    return this.level;
+  }
+
+  throwDart() {
+    let actualPoint: number = 0;
+    if (this.level < 2 || this.isFortunate===false || this.alcoholLevel >2){
+    actualPoint = Math.floor((Math.random()*20)*Math.random())    
+    } else if (this.isFortunate===true && this.alcoholLevel>2 ) {
+      actualPoint = Math.floor((Math.random()*20))*3
+    } else {
+      actualPoint =  Math.floor((Math.random()*20))*Math.floor(Math.random()*3)
+    }
+    return actualPoint
+  }
+}
---
+++ week-04/day-05/demo/program.ts
@@ -0,0 +1,12 @@
+import { Player } from "./player";
+
+let testDisneyGirl: Player = new Player('Cinderella');
+testDisneyGirl.playerInfoCard()
+
+console.log(testDisneyGirl.throwDart())
+console.log(testDisneyGirl.throwDart())
+console.log(testDisneyGirl.throwDart())
+console.log(testDisneyGirl.throwDart())
+console.log(testDisneyGirl.throwDart())
+
+testDisneyGirl.playerInfoCard()
