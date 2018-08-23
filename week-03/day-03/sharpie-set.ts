c30a745d6b87c7243ae5d7e67d3914f45e296fec
PKriszta <parrag.krisztina@gmail.com>
2018-08-23 10:17:15 +0200

    (refact) testing removeTrash method with filter and splice then keeping splice because it serves the purpose better

--- week-03/day-03/sharpie-set.ts
+++ week-03/day-03/sharpie-set.ts
@@ -18,21 +18,21 @@ class SharpieSet {
     return usableNo;
   }
   
-  //MIERT NEM MUKODIK FILTERREL????
-  removeTrash() {
-    return this.sharpieList.filter(function (element) {
-      return element.inkAmount > 0
-    })
-  }
-
+ // filterrel tudom returnolni de nem valtoztatja meg az alap arrayt ezert nem jo
   // removeTrash() {
-  //   for (let i = 0; i < this.sharpieList.length; i++) {
-  //     if (this.sharpieList[i].getInkAmount()===0) {
-  //       this.sharpieList.splice(i, 1);
-  //       i--;
-  //     }      
-  //   }
+  //   return this.sharpieList.filter(function (element) {
+  //     return element.inkAmount > 0
+  //   })
   // }
+
+  removeTrash() {
+    for (let i = 0; i < this.sharpieList.length; i++) {
+      if (this.sharpieList[i].getInkAmount()===0) {
+        this.sharpieList.splice(i, 1);
+        i--;
+      }      
+    }
+  }
 }
 
 
@@ -46,16 +46,16 @@ for (let index = 0; index < 10; index++) {
 }
 console.log(sharpieSetTest);
 
-//using one sharpie fully
+// using one sharpie fully
 for (let index = 0; index < 100; index++) {
   sharpieSetTest.sharpieList[7].use();
 }
-console.log(sharpieSetTest.sharpieList[7]);
+// console.log(sharpieSetTest.sharpieList[7]);
 
 console.log(sharpieSetTest.countUsable());
 
-console.log(sharpieSetTest.removeTrash());
-// console.log(sharpieSetTest);
+sharpieSetTest.removeTrash();
+console.log(sharpieSetTest);
 
 
 
