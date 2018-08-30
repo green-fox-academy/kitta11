6229ac06a0649a1ea6bd4b68058ee879176a5b4e
PKriszta <parrag.krisztina@gmail.com>
2018-08-30 21:50:40 +0200

    (feat) poem text counter exluding special characters

--- week-05/day-04/poem/poem-counter.ts
+++ week-05/day-04/poem/poem-counter.ts
@@ -1,21 +1,35 @@
 import { readFromFile } from "../../../library/readfromfile";
 'use strict'
 
+export function clearString(inputfile) {
+  let specialChar: string[] = [`+`, `-`, `&`, `!`, `(`, `)`, `{`, `}`, `[`, `]`, `^`, `"`, `~`, `*`, `?`, `:`, `.`, `,`]
+  let dirtyString: string = readFromFile(inputfile)
+
+  specialChar.forEach(element => {
+    while (dirtyString.indexOf(element) > 0) {
+      dirtyString = dirtyString.replace(element, '')
+    }
+  });
+
+  return dirtyString.split('\n').join(' ').split(' ')
+}
+
 function poemAvgWordLength(inputfile: string) {
-  let tempArray= readFromFile(inputfile).split('\n').join(' ').split(' ')
+  let clearedArray: string[] = clearString(inputfile)
 
   let totalChar: number = 0;
-  tempArray.forEach(element => {
-    totalChar+=element.length    
+  clearedArray.forEach(element => {
+    totalChar += element.length
   });
-  return Math.floor(totalChar/tempArray.length*100)/100
+  return Math.floor(totalChar / clearedArray.length * 100) / 100
 }
+
 console.log(poemAvgWordLength('poem.txt'))
 
-function poemWordsLongerThan (inputfile: string, lengthNumber: number) {
-  return (readFromFile(inputfile).split('\n').join(' ').split(' ').filter(function(element) {
+function poemWordsLongerThan(inputfile: string, lengthNumber: number) {
+  return clearString(inputfile).filter(function (element) {
     return element.length >= lengthNumber
-  })).length
+  }).length
 }
 
 console.log(poemWordsLongerThan('poem.txt', 10))
