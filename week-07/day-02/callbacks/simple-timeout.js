e244e37acce410b9c405f411e0f24c5ebd336a10
PKriszta <parrag.krisztina@gmail.com>
2018-09-18 16:32:36 +0200

    simple timeout

---
+++ week-07/day-02/callbacks/simple-timeout.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Write a program that prints apple after 3 seconds.
+
+setTimeout(() => {
+  console.log('apple'); // prints after 3 seconds
+}, 3000);
+
+// const timer = setTimeout(() => {
+//   console.log('apple'); // prints after 3 seconds
+// }, 3000);
