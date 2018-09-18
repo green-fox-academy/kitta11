15f7767646a5cbdd5c919c69a87b75605a712d20
PKriszta <parrag.krisztina@gmail.com>
2018-09-18 09:04:29 +0200

    (feat) eng version implemented using Hypher npm modul

--- week-07/day-01/hcs/app.js
+++ week-07/day-01/hcs/app.js
@@ -10,23 +10,43 @@ app.get('/', (req, res) => {
   res.send('todo va bien')
 })
 
-function cammelizer(inputString) {
-  let myRegexp = /[a,á,e,é,i,í,o,ó,ö,ő,u,ú,ü,ű]/i;
-  let camellizedArray = inputString.split('').map(elem => {
-    if (myRegexp.test(elem)) {
-      return elem + 'v' + elem;
-    } else {
-      return elem;
-    }
-  })
-  return camellizedArray.join('')
+let Hypher = require('hypher'),
+  english = require('hyphenation.en-us'),
+  h = new Hypher(english);
 
+
+function cammelizer(inputString, language) {
+  if (language === 'hu') {
+    let myRegexp = /[a,á,e,é,i,í,o,ó,ö,ő,u,ú,ü,ű]/i;
+    let camellizedArray = inputString.split('').map(elem => {
+      if (myRegexp.test(elem)) {
+        return elem + 'v' + elem;
+      } else {
+        return elem;
+      }
+    })
+    return camellizedArray.join('')
+  } else if (language === 'eng') {
+    let hyphenated = h.hyphenate(inputString)
+    const gibberish = ['idig', 'uddag', 'uvug', 'uthug'];
+
+    let camellizedArray = hyphenated.map(elem => {
+      return elem + gibberish[Math.floor(Math.random() * 4)]
+    })
+    return camellizedArray.join('')
+
+  }
 }
 
 app.post('/translate', jsonParser, (req, res) => {
   if (req.body.lang === 'hu') {
     res.json({
-      "translated": cammelizer(req.body.text),
+      "translated": cammelizer(req.body.text, 'hu'),
+      "lang": "teve"
+    })
+  } else if (req.body.lang === 'eng') {
+    res.json({
+      "translated": cammelizer(req.body.text, 'eng'),
       "lang": "teve"
     })
   } else {
