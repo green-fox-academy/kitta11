f2d43962214343950eba9a6c3884460fbe23f577
PKriszta <parrag.krisztina@gmail.com>
2018-09-23 12:42:27 +0200

    feat: onclick feature added to show moving gif

--- week-07/day-02/giphy/assets/js/script.js
+++ week-07/day-02/giphy/assets/js/script.js
@@ -2,27 +2,66 @@ window.onload = () => {
   const host = 'http://localhost:3000'
   const http = new XMLHttpRequest();
   const parentDiv = document.querySelector('#giphylist');
+  const btnSelector = document.querySelectorAll('#selector button');
+  console.log(btnSelector);
+  let searchPhrase = 'sloth';
 
-  http.open('GET', `${host}/api/giphy/sloth`, true);
+  //not working yet
+  btnSelector.forEach(button => {
+    button.addEventListener('click', (e) => {
+      searchPhrase = button.innerText;
+      http.open('GET', `${host}/api/giphy/${searchPhrase}`, true);
+      console.log(http);
+    })
+  })
+
+
+  http.open('GET', `${host}/api/giphy/${searchPhrase}`, true);
   console.log(http);
 
   http.onload = () => {
+
     if (http.status === 200) {
       const source = JSON.parse(http.response).data
       console.log(source);
       source.forEach((elem) => {
-        let giflink = document.createElement('a')
-        parentDiv.appendChild(giflink)
-        giflink.setAttribute('href', elem.bitly_url)
-        giflink.setAttribute('target', 'blank')
+        let gifImgCont = document.createElement('div')
+        parentDiv.appendChild(gifImgCont);
+        gifImgCont.id = 'gifImgCont';
+
+        let gifTitle = document.createElement('div')
+        gifImgCont.appendChild(gifTitle);
+        gifTitle.innerText = elem.title;
+        gifTitle.id = 'gifTitle';
+
         let gifImg = document.createElement('img');
-        giflink.appendChild(gifImg)
-        gifImg.classList.add('listview')
-        gifImg.setAttribute('src', elem.images['480w_still'].url)
+        gifImgCont.appendChild(gifImg);
+        gifImg.classList.add('listview');
+        gifImg.setAttribute('id', elem.id);
+        gifImg.setAttribute('src', elem.images['480w_still'].url);
+        gifImg.setAttribute('alt', 'Click to see the gif moving');
+
+        gifImg.addEventListener('mouseover', () => {
+          gifImg.title = 'Click to see the gif moving';
+        })
+
+        gifImg.addEventListener('click', () => {
+          if (!gifImg.classList.contains('clicked')) {
+            gifImg.setAttribute('src', elem.images.downsized_medium.url);
+            gifImg.classList.add('clicked');
+            console.log(gifImg);
+          } else {
+            gifImg.classList.remove('clicked');
+            gifImg.setAttribute('src', elem.images['480w_still'].url);
+            console.log(gifImg);
+          }
+        })
 
       })
     }
-  };
+  }
+
+
 
   http.send();
 
