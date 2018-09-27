f5ce67c198ba130bace6ecbd42878d25533181f1
PKriszta <parrag.krisztina@gmail.com>
2018-09-27 16:04:49 +0200

    catfact subs event listener

--- week-08/day-04/buttondisable/index.js
+++ week-08/day-04/buttondisable/index.js
@@ -1,13 +1,21 @@
 'use strict'
 window.onload = () => {
   let submitBtn = document.querySelector("button[type=submit]");
+  let loveCatsBtn = document.querySelector("button[type=button]");
   let favpetDiv = document.querySelector("#favpetdiv");
+  let catfactsub = document.querySelector("#catfactsub");
+
 
   favpetDiv.addEventListener('change', (e) => {
-    submitBtn.disabled = false
-
+    submitBtn.disabled = false;
   })
 
-
-
+  catfactsub.addEventListener('change', (e) => {
+    if (e.target.value === 'yes') {
+      loveCatsBtn.disabled = false;
+      alert(`Thank you, you've successfully signed up for cat facts`)
+    } else {
+      alert(`Sigh, we still added you to the cat facts list`)
+    }
+  })
 }
