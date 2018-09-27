67549a3ad85a975c07176c49ec856d7b88cbf729
PKriszta <parrag.krisztina@gmail.com>
2018-09-27 17:21:52 +0200

    feat: select logic with dependencies implemented without css

---
+++ week-08/day-04/movieselect/index.js
@@ -0,0 +1,36 @@
+'use strict';
+window.onload = () => {
+
+  let genrelistDIV = document.querySelector('#genrelist');
+  let movielistDIV = document.querySelector('#movielist');
+  let movielist = document.querySelector('#movies').querySelectorAll('option');
+  let selectedSpan = document.querySelector('#selected');
+
+
+  genrelistDIV.addEventListener('change', (e) => {
+    if (e.target.value === "Sci-fi") {
+      movielist.forEach(movie => {
+        if (movie.classList.contains("scifi")) {
+          movie.disabled = false
+        }
+      })
+    } else if (e.target.value === "Drama") {
+      movielist.forEach(movie => {
+        if (movie.classList.contains("drama")) {
+          movie.disabled = false
+        };
+      })
+    } else if (e.target.value === "Comedy") {
+      movielist.forEach(movie => {
+        if (movie.classList.contains("comedy")) {
+          movie.disabled = false
+        };
+      })
+    }
+  })
+  movielistDIV.addEventListener('change', (e) => {
+    console.log(e.target.value)
+    selectedSpan.innerText = e.target.value;
+  })
+
+}
