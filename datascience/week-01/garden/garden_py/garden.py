a1ee9deb8089e0cbde823b64655355d6758dbb5e
PKriszta <parrag.krisztina@gmail.com>
2018-10-11 15:12:59 +0200

    coderefactor

--- datascience/week-01/garden/garden_py/garden.py
+++ datascience/week-01/garden/garden_py/garden.py
@@ -12,28 +12,21 @@ class Garden:
     def addToGarden(self, item):
         self._plants.append(item)
 
+    def doesneedwater(self, item):
+        return item.needWater()
+
     def watering(self, totalWater):
         print("Watering with:", totalWater)
-        needsWater = []
-
-        #  for count, item in enumerate(self._plants):
-        #       if (item.needWater()):
-        # needsWater.append(item)
-        # for item in enumerate(self._plants):
-        #     if (item.needWater()):
-        #         item.beWatered(totalWater/len(needsWater))
         for i in range(len(self._plants)):
             if (self._plants[i].needWater()):
-                needsWater.append(self._plants[i])
-        for i in range(len(self._plants)):
-            if (self._plants[i].needWater()):
-                self._plants[i].beWatered(totalWater/len(needsWater))
+                self._plants[i].beWatered(
+                    totalWater/len(list(filter(self.doesneedwater, self._plants))))
 
     def printGarden(self):
         for i in range(len(self._plants)):
             if (self._plants[i].needWater()):
-                print("The %s %s needs water" %
-                      (self._plants[i].get_color(), self._plants[i].get_type()))
+                print(
+                    f"The {self._plants[i].get_color()}  {self._plants[i].get_type()} needs water ")
             else:
-                print("The %s %s does not need water" %
-                      (self._plants[i].get_color(), self._plants[i].get_type()))
+                print(
+                    f"The {self._plants[i].get_color()}  {self._plants[i].get_type()} does not need water ")
