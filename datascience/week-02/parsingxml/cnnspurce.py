cc7944f174512b634a3fb3a53c96104aa2ce9725
PKriszta <parrag.krisztina@gmail.com>
2018-10-16 10:27:04 +0200

    moving in the nodetree

--- datascience/week-02/parsingxml/cnnspurce.py
+++ datascience/week-02/parsingxml/cnnspurce.py
@@ -9,20 +9,23 @@ cnnsource = requests.get(
     'http://rss.cnn.com/rss/edition_technology.rss')
 
 soup = bs4.BeautifulSoup(cnnsource.text, 'xml')
-
-items = soup.find_all('item')
-print(items)
-
 root = ET.fromstring(cnnsource.text)
 
+
+items = soup.find_all('item')
+# print(items)
+
+for item in items:
+    print(item.title)
+
 walkAll = root.getiterator('item')
-print(walkAll.item)
-for elt in walkAll:
-    print(elt)
 
-for elt in walkAll:
-    print(elt.attrib)
-    print(elt.tag)
+# for elt in walkAll:
+#     print(elt)
 
-for child in root:
-    print(child.tag, child.attrib)
+# for elt in walkAll:
+#     print(elt.attrib)
+#     print(elt.tag)
+
+# for child in walkAll:
+#     print((child.tag).child)
