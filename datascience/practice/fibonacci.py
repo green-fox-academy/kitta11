06002309ae45f2bbde98ec791510bf2e3be9fc7b
PKriszta <parrag.krisztina@gmail.com>
2018-11-20 06:47:41 +0100

    testing fibonacci calculation with return and yield

---
+++ datascience/practice/fibonacci.py
@@ -0,0 +1,30 @@
+import time
+
+start_time = time.time()
+
+
+def fibonacci(n):
+    result = [0, 1]
+    for i in range(2, n):
+        result.append(result[i-2]+result[i-1])
+    return result
+
+
+def fibonacci_yield():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a+b
+
+
+print(fibonacci(10))
+end1 = time.time()
+
+f = fibonacci_yield()
+f.__next__()
+for x in range(10):
+    print(f.__next__())
+end2 = time.time()
+
+print('with array:', end2 - start_time)
+print('with yield:', end1 - start_time)
