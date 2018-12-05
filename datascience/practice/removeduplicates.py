3decea826e1350069fbba9ce77a8fc9ad61830fc
PKriszta <parrag.krisztina@gmail.com>
2018-12-05 06:34:22 +0100

    extended with multiple duplicates

--- datascience/practice/removeduplicates.py
+++ datascience/practice/removeduplicates.py
@@ -17,3 +17,30 @@ def Remove(duplicate):
 
 print(notduplicated(sampleinput))
 print(Remove(sampleinput))
+
+
+def duplicates(duplicate):
+    duplicated_list = []
+    final_list = []
+    for num in duplicate:
+        if num not in final_list:
+            final_list.append(num)
+        else:
+            duplicated_list.append(num)
+    return duplicated_list
+
+
+print(duplicates(sampleinput))
+
+
+def find_dupla(duplicate):
+    dict = {}
+    for item in duplicate:
+        if item in dict:
+            dict[item] += 1
+        else:
+            dict[item] = 1
+    return [key for key in dict if dict[key] > 1]
+
+
+print(find_dupla(sampleinput))
